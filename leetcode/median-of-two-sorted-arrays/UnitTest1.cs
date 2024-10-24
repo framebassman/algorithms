a886e2315aa19de2bd292e1741f28b9d144ed16a
Dmitry Romashov <framebassman@gmail.com>
2024-10-24 18:55:26 +0200

    Dirty solution

--- leetcode/median-of-two-sorted-arrays/UnitTest1.cs
+++ leetcode/median-of-two-sorted-arrays/UnitTest1.cs
@@ -4,6 +4,19 @@ public class UnitTest1
 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
+        if (nums1.Length == 0) {
+            return GetMedian(nums2);
+        }
+        
+        if (nums2.Length == 0) {
+            return GetMedian(nums1);
+        }
+        
+        var result = MergeArrays(nums1, nums2);
+        return GetMedian(result);
+    }
+    
+    public static int[] MergeArrays(int[] nums1, int[] nums2) {
         var result = new int[nums1.Length + nums2.Length];
         
         var firstIndex = 0;
@@ -19,26 +32,71 @@ public class UnitTest1
             else
             {
                 result[i] = nums2[secondIndex];
-                secondIndex++;
+                if (secondIndex < nums2.Length - 1) {
+                    secondIndex++;
+                }
             }
         }
-        
-        if (result.Length % 2 == 0)
-        {
-            return result[(result.Length / 2) - 1] / 2.0;
+
+        return result;
+    }
+    
+    public static double GetMedian(int[] result) {
+        if (result.Length == 0) {
+            return 0;
         }
-        else
+        
+        if (result.Length == 1) {
+            return result[0];
+        }
+        
+        if (result.Length == 2) {
+            return ((double)result[0] + (double)result[1]) / 2.0;
+        }
+
+        if (result.Length % 2 == 0)
         {
             return (result[(result.Length / 2) - 1] + result[result.Length / 2]) / 2.0;
         }
+        else
+        {
+            return result[result.Length / 2];
+        }
     }
-    
+
     [Fact]
     public void Test1()
     {
         var test = new UnitTest1();
         int[] arr1 =  new int[] { 1, 2, 4};
         int[] arr2 = new int[] {3, 5};
+        Assert.Equal(3.0, test.FindMedianSortedArrays(arr1, arr2));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var test = new UnitTest1();
+        int[] arr1 =  new int[] { 1, 3};
+        int[] arr2 = new int[] {2};
+        Assert.Equal(2.0, test.FindMedianSortedArrays(arr1, arr2));
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var test = new UnitTest1();
+        int[] arr1 =  new int[] { 1, 3};
+        int[] arr2 = new int[] {2, 4};
         Assert.Equal(2.5, test.FindMedianSortedArrays(arr1, arr2));
+    }
+    
+    [Fact]
+    public void Test5()
+    {
+        var test = new UnitTest1();
+        int[] arr1 = new int[] { 100_001 };
+        int[] arr2 = new int[] { 100_000 };
+        Assert.Equal(100_000.5, test.FindMedianSortedArrays(arr1, arr2));
     }
 }
