e99c323fd6a6d0cc3e0515e112cc31ec29b539cc
Dmitry Romashov <framebassman@gmail.com>
2024-12-04 23:14:26 +0100

    Add comments

--- hackerrank/counting-binary-substrings/UnitTest1.cs
+++ hackerrank/counting-binary-substrings/UnitTest1.cs
@@ -44,6 +44,21 @@ public class UnitTest1
         Xunit.Assert.Equal(1, counting("011"));
         Xunit.Assert.Equal(2, counting("010"));
         Xunit.Assert.Equal(4, counting("001101"));
-        Xunit.Assert.Equal(10, counting("10010011100011"));
+        Xunit.Assert.Equal(11, counting("10010011100011"));
+        //                               1 0 0 1 0 0 1 1 1 0 0 0 1 1
+        //                               0 1 2 3 4 5 6 7 8 9 10111213 
+        // 10010011100011
+        // 0-1 - 1
+        // 0-3 - 2
+        // 2-3 - 3
+        // 3-4 - 4
+        // 3-6 - 5
+        // 4-7 - 6
+        // 5-6 - 7
+        // 7-10 - 8
+        // 8-9 - 9
+        // 10-13 - 10
+        // 11-12 - 11
+        // 
     }
 }
